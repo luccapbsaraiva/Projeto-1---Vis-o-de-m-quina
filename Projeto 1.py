ac2486bc23cbfca158481b65e104453b8daf2cfa
luccapbsaraiva <luccapbs@al.insper.edu.br>
2024-04-09 16:25:49 -0300

    carrega primeiras imagens

---
+++ Projeto 1.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+boa1 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_01.png', 1)
+boa2 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_02.png', 1)
+boa3 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_03.png', 1)
+boa4 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_04.png', 1)
+boa5 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_05.png', 1)
