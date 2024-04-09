bcc121675d13b398edf9595e6f47d3ea7d52c36e
luccapbsaraiva <luccapbs@al.insper.edu.br>
2024-04-09 16:43:25 -0300

    imagens carregadas

--- Projeto 1.py
+++ Projeto 1.py
@@ -2,8 +2,27 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-boa1 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_01.png', 1)
-boa2 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_02.png', 1)
-boa3 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_03.png', 1)
-boa4 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_04.png', 1)
-boa5 = cv2.imread('Imagens_DSENVOLVIMENTO/_boa_05.png', 1)
+boa1 = cv2.imread('Imagens_DESENVOLVIMENTO/_boa_01.png', 1)
+boa2 = cv2.imread('Imagens_DESENVOLVIMENTO/_boa_02.png', 1)
+boa3 = cv2.imread('Imagens_DESENVOLVIMENTO/_boa_03.png', 1)
+boa4 = cv2.imread('Imagens_DESENVOLVIMENTO/_boa_04.png', 1)
+boa5 = cv2.imread('Imagens_DESENVOLVIMENTO/_boa_05.png', 1)
+
+
+amassada1 = cv2.imread('Imagens_DESENVOLVIMENTO/amassada1.png', 1)
+amassada2 = cv2.imread('Imagens_DESENVOLVIMENTO/amassada2.png', 1)
+amassada3 = cv2.imread('Imagens_DESENVOLVIMENTO/amassada3.png', 1)
+
+color1 = cv2.imread('Imagens_DESENVOLVIMENTO/color1.png',1)
+color3  = cv2.imread('Imagens_DESENVOLVIMENTO/color3.png',1)
+
+quebrada1 = cv2.imread('Imagens_DESENVOLVIMENTO/quebrada_01.png',1)
+quebrada2 = cv2.imread('Imagens_DESENVOLVIMENTO/quebrada_02.png',1)
+quebrada3 = cv2.imread('Imagens_DESENVOLVIMENTO/quebrada_03.png',1)
+
+riscada1 = cv2.imread('Imagens_DESENVOLVIMENTO/riscada1.png',1)
+riscada2 = cv2.imread('Imagens_DESENVOLVIMENTO/riscada2.png',1)
+
+plt.imshow(boa2)
+plt.show()
+
